c7e0e91e8b59039d1a0608be039abb7fe88af56d
Cássia Sanctos <csamp@ibm.com>
2024-08-08 22:12:34 -0300

    adding app, config, sentences json and static files

---
+++ app.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Copyright 2021, IBM Corporation. 
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+Flask API app and routes.
+"""
+
+__author__ = "Vagner Santana, Melina Alberio, Cassia Sanctos and Tiago Machado"
+__copyright__ = "IBM Corporation 2024"
+__credits__ = ["Vagner Santana, Melina Alberio, Cassia Sanctos, Tiago Machado"]
+__license__ = "Apache 2.0"
+__version__ = "0.0.1"
+
+from flask import Flask, jsonify, request
+from flask_cors import CORS, cross_origin
+from flask_restful import Resource, Api, reqparse
+import control.recommendation_handler as recommendation_handler 
+import helpers.get_credentials as get_credentials
+import config as cfg
+
+app = Flask(__name__)
+
+# swagger configs
+app.register_blueprint(cfg.SWAGGER_BLUEPRINT, url_prefix = cfg.SWAGGER_URL)
+
+@app.route("/")
+def index():
+    return "Ready!"
+
+@app.route("/recommend", methods=['GET'])
+@cross_origin()
+def recommend():
+    api_url, headers = get_credentials.get_credentials()
+    prompt_json = recommendation_handler.populate_json()
+    args = request.args
+    print("args list = ", args)
+    prompt = args.get("prompt")
+    recommendation_json = recommendation_handler.recommend_prompt(prompt, prompt_json, api_url, headers)
+    return recommendation_json
+        
+if __name__=='__main__':
+	app.run(host='0.0.0.0', port='8080', debug=True)
---
+++ config.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Copyright 2021, IBM Corporation. 
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+Swagger configuration.
+"""
+
+__author__ = "Vagner Santana, Melina Alberio, Cassia Sanctos and Tiago Machado"
+__copyright__ = "IBM Corporation 2024"
+__credits__ = ["Vagner Santana, Melina Alberio, Cassia Sanctos, Tiago Machado"]
+__license__ = "Apache 2.0"
+__version__ = "0.0.1"
+
+from flask_swagger_ui import get_swaggerui_blueprint
+
+SWAGGER_URL = '/swagger'
+API_URL = '/static/swagger.json'
+SWAGGER_BLUEPRINT = get_swaggerui_blueprint(
+    SWAGGER_URL,
+    API_URL,
+    config={
+        'app_name': "Prompt Recommendation API"
+    }
+)
